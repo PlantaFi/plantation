7744d7ad0789e4db42b722b8552a2dd2d57f9b3e
sam <s.elhamy@gmail.com>
2021-11-26 17:43:45 +0100

    task #26
    Create a basic shopUI & bankUI

--- packages/react-app/src/components/FruitSwap.jsx
+++ packages/react-app/src/components/FruitSwap.jsx
@@ -1,15 +1,10 @@
 import { utils } from "ethers";
 import { Button, Divider, Input } from "antd";
 import React, { useState } from "react";
-import {
-  useContractLoader,
-  useContractReader,
-  useUserProviderAndSigner,
-} from "eth-hooks";
+import { useContractLoader, useContractReader, useUserProviderAndSigner } from "eth-hooks";
 
 const fmtEth = utils.formatEther;
 const parseEth = utils.parseEther;
-
 
 function _onUpdate(update) {
   console.log("📡 Transaction Update:", update);
@@ -17,71 +12,66 @@ function _onUpdate(update) {
     console.log(" 🍾 Transaction " + update.hash + " finished!");
     console.log(
       " ⛽️ " +
-      update.gasUsed +
-      "/" +
-      (update.gasLimit || update.gas) +
-      " @ " +
-      parseFloat(update.gasPrice) / 1000000000 +
-      " gwei",
+        update.gasUsed +
+        "/" +
+        (update.gasLimit || update.gas) +
+        " @ " +
+        parseFloat(update.gasPrice) / 1000000000 +
+        " gwei",
     );
   }
 }
 
-function FMaticOut({ fruitIn, readContracts, }) {
+function FMaticOut({ fruitIn, readContracts }) {
   const fmaticOut = useContractReader(readContracts, "Fruniswap", "getAmountOutForFruitIn", [parseEth(fruitIn)]);
-  return (<span>{fmaticOut ? fmtEth(fmaticOut) : '...'}</span>);
+  return <span>{fmaticOut ? fmtEth(fmaticOut) : "..."}</span>;
 }
-export default function FruitSwap({
-  address,
-  tx,
-  readContracts,
-  writeContracts,
-}) {
+
+export default function FruitSwap({ address, tx, readContracts, writeContracts }) {
   const [newFruitAmount, setNewFruitAmount] = useState("1");
+    const [fruitIn, setFruitIn] = useState("1");
   const [newFMaticAmount, setNewFMaticAount] = useState(parseEth("0"));
-  const fruniswapFruitAllow = useContractReader(readContracts, "Fruit", "allowance", [address, writeContracts.Fruniswap.address]);
-  const fruniswapFMaticAllow = useContractReader(readContracts, "FMatic", "allowance", [address, writeContracts.Fruniswap.address]);
-
-
+  const fruniswapFruitAllow = useContractReader(readContracts, "Fruit", "allowance", [
+    address,
+    writeContracts.Fruniswap.address,
+  ]);
+  const fruniswapFMaticAllow = useContractReader(readContracts, "FMatic", "allowance", [
+    address,
+    writeContracts.Fruniswap.address,
+  ]);
+  const fmaticOut = useContractReader(readContracts, "Fruniswap", "getAmountOutForFruitIn", [parseEth(newFruitAmount)]);
+  console.log(newFruitAmount +" newFruitAmount");
   return (
     <div>
       {/*
         ⚙️ Here is an example UI that displays and sets the purpose in your smart contract:
       */}
-        <div style={{}}>
+      <div style={{}}>
+        <div>
           <div>
-            <div>
-              {newFruitAmount} FRUIT gets <FMaticOut fruitIn={newFruitAmount} readContracts={readContracts} /> FMATIC
-            </div>
+            {newFruitAmount} FRUIT gets <span>{fmaticOut ? fmtEth(fmaticOut) : "..."}</span> FMATIC
           </div>
-          <Input
-            value={newFruitAmount}
-            onChange={async(e) => {
-              try {
-                parseEth(e.target.value);
-                setNewFruitAmount(e.target.value);
+        </div>
+        <Input
+        placeholder="Enter Number of Fruits"
+          defaultValue={newFruitAmount}
+          onChange={async e => {
+            try {
+              parseEth(e.target.value);
+              setNewFruitAmount(e.target.value);
 
-                setNewFMaticAount(await readContracts.Fruniswap.getAmountInForFruitOut(parseEth(e.target.value)));
-                console.log(x);
-              } catch(err) {
-              }
-            }}
-          />
-    {/*
+
+              console.log(x);
+            } catch (err) {}}}
+        />
+        {/*
           <div>
             check FRUIT allowance {fruniswapFruitAllow ? fmtEth(fruniswapFruitAllow) : '...'} &gt;= {newFruitAmount}
           </div>
      */}
+        {fruniswapFruitAllow > 0 ? (
           <Button
-            style={{ marginTop: 8 }}
-            onClick={async () => {
-              const result = tx(writeContracts.Fruit.approve(writeContracts.Fruniswap.address, parseEth(newFruitAmount)), _onUpdate);
-              console.log(await result);
-            }}
-          >
-            Approve Fruit
-          </Button>
-          <Button className='nes-btn is-error'
+            className="nes-btn is-error"
             style={{ marginTop: 8 }}
             onClick={async () => {
               const result = tx(writeContracts.Fruniswap.sellFruit(parseEth(newFruitAmount)), _onUpdate);
@@ -90,25 +80,31 @@ export default function FruitSwap({
           >
             Sell Fruit
           </Button>
-        </div>
-        <div>
-    {/*
+        ) : (
+          <Button
+            style={{ marginTop: 8 }}
+            onClick={async () => {
+              const result = tx(
+                writeContracts.Fruit.approve(writeContracts.Fruniswap.address, parseEth(newFruitAmount)),
+                _onUpdate,
+              );
+              console.log(await result);
+            }}
+          >
+            Approve Fruit
+          </Button>
+        )}
+      </div>
+      <div>
+        {/*
           <div>
             check FMATIC allowance {fruniswapFMaticAllow ? fmtEth(fruniswapFMaticAllow) : '...'} &gt;= {fmtEth(newFMaticAmount)} (cost in FMATIC to buy FRUIT amount entered)
           </div>
      */}
+        {fruniswapFMaticAllow > 0 ? (
           <Button
-            style={{ }}
-            onClick={async () => {
-              const result = tx(writeContracts.FMatic.approve(writeContracts.Fruniswap.address, newFMaticAmount), _onUpdate);
-              console.log("awaiting metamask/web3 confirm result...", result);
-              console.log(await result);
-            }}
-          >
-            Approve FMATIC
-          </Button>
-          <Button className='nes-btn is-success'
-            style={{ }}
+            className="nes-btn is-success"
+            style={{ marginTop: 8 }}
             onClick={async () => {
               const result = tx(writeContracts.Fruniswap.buyFruit(parseEth(newFruitAmount)), _onUpdate);
               console.log("awaiting metamask/web3 confirm result...", result);
@@ -117,7 +113,35 @@ export default function FruitSwap({
           >
             Buy Fruit
           </Button>
-        </div>
+        ) : (
+          <Button
+            style={{ marginTop: 8 }}
+            onClick={async () => {
+              const result = tx(
+                writeContracts.FMatic.approve(writeContracts.Fruniswap.address, newFMaticAmount),
+                _onUpdate,
+              );
+              console.log("awaiting metamask/web3 confirm result...", result);
+              console.log(await result);
+            }}
+          >
+            Approve FMATIC
+          </Button>
+        )}
       </div>
+    </div>
   );
 }
+/*
+{async e => {
+  try {
+    parseEth(e.target.value);
+    setNewFruitAmount(e.target.value);
+
+    setNewFMaticAount(await readContracts.Fruniswap.getAmountInForFruitOut(parseEth(e.target.value)));
+    console.log(x);
+  } catch (err) {}
+}}
+
+<FMaticOut fruitIn={newFruitAmount} readContracts={readContracts} />
+*/
--- packages/react-app/src/views/Banks.jsx
+++ packages/react-app/src/views/Banks.jsx
@@ -99,15 +99,12 @@ function GetApproveFruit({ address, readContracts, plantAddress, setDisplayBuyPl
 
 function Shop({ address, tx, readContracts, writeContracts, setTransferEvents }) {
   const [buyPlantBtnStr, setBuyPlantBtnStr] = useState("Buy Seed");
-  const [buyFruitBtnStr, setBuyFruitBtnStr] = useState("Get Free Fruits");
   const [displayBuyPlantBtn, setDisplayBuyPlantBtn] = useState(false);
   const [buyPlantStr, setBuyPlantStr] = useState("");
 
   const seedPrice = useContractReader(readContracts, "Plant", "currentPrice");
   const fruitBalance = useContractReader(readContracts, "Fruit", "balanceOf", [address]);
   let displayApproveBtn = false;
-  let displayBuyFruitBtn = true;
-  // let displayBuyPlantBtn = true;
 
   if (seedPrice != undefined) {
     const plantAddress = readContracts.Plant.address;
@@ -139,7 +136,7 @@ function Shop({ address, tx, readContracts, writeContracts, setTransferEvents })
 
       <div className="nes-container is-rounded is-dark with-title" style={{maxWidth: 400, display: 'inline-block', verticalAlign: 'top'}}>
         <p className="title">Fruit</p>
-        {writeContracts.Fruit ? (<FruitSwap
+        {writeContracts.Fruit !=undefined ? (<FruitSwap
             address={address}
             tx={tx}
             writeContracts={writeContracts}
@@ -149,39 +146,6 @@ function Shop({ address, tx, readContracts, writeContracts, setTransferEvents })
       </div>
       <div className="nes-container is-rounded is-dark with-title" style={{maxWidth: 400, display: 'inline-block', verticalAlign: 'top'}}>
         <p className="title">Seed</p>
-        <div>Fruit Owned:{fruitBalance ? utils.formatEther(fruitBalance) : "loading..."} </div>
-        {displayBuyFruitBtn ? (
-          <Button
-            style={{ marginTop: 8, marginBottom: 8 }}
-            onClick={async () => {
-              setBuyFruitBtnStr("loading...");
-
-              const result = tx(writeContracts.Fruit.freeFruit(), update => {
-                console.log("📡 Transaction Update:", update);
-                if (update && (update.status === "confirmed" || update.status === 1)) {
-                  console.log(" 🍾 Transaction " + update.hash + " finished!");
-                  console.log(
-                    " ⛽️ " +
-                      update.gasUsed +
-                      "/" +
-                      (update.gasLimit || update.gas) +
-                      " @ " +
-                      parseFloat(update.gasPrice) / 1000000000 +
-                      " gwei",
-                  );
-                }
-              });
-              console.log("awaiting metamask/web3 confirm result...", result);
-              console.log(await result);
-              setBuyFruitBtnStr("Get Free Fruits");
-            }}
-          >
-            {buyFruitBtnStr}
-          </Button>
-        ) : (
-          ""
-        )}
-        <br />
         {seedPrice ? (
           <GetApproveFruit
             address={address}
@@ -254,16 +218,6 @@ export default function Banks({
   readContracts,
   writeContracts,
 }) {
-  const landSupply = useContractReader(readContracts, "Land", "totalSupply");
-  const crcBalance = useContractReader(readContracts, "Currency", "balanceOf", [address]);
-
-  const seedBalance = useContractReader(readContracts, "Seed", "balanceOf", [address]);
-  const landBalance = useContractReader(readContracts, "Land", "balanceOf", [address]);
-
-  const fruitSupply = useContractReader(readContracts, "Fruit", "totalSupply");
-
-  console.log("setTransferEvents " + setTransferEvents.length + " " + plantsCount);
-  // plantsCount = ;
 
   return (
     <div>
@@ -271,7 +225,7 @@ export default function Banks({
       ⚙️ Here is an example UI that displays and sets the purpose in your smart contract:
     */}
 
-      {writeContracts.Fruit ? (<PlantaWallet
+      {writeContracts.Fruit != undefined ? (<PlantaWallet
         readContracts={readContracts}
         writeContracts={writeContracts}
         tx={tx}
@@ -286,38 +240,4 @@ export default function Banks({
       />
     </div>
   );
-  // <table style={{ margin: "auto" }}>
-  //   <thead>
-  //     <tr>
-  //       <th>Token</th>
-  //       <th>Balance</th>
-  //     </tr>
-  //   </thead>
-  //   <tbody>
-  //     <tr>
-  //       <td>CRC</td>
-  //       <td>{crcBalance ? utils.formatEther(crcBalance) : "..."}</td>
-  //     </tr>
-  //     <tr>
-  //       <td>SEED</td>
-  //       <td>{seedBalance ? seedBalance.toNumber() : "..."}</td>
-  //     </tr>
-  //     <tr>
-  //       <td>LAND</td>
-  //       <td>{landBalance ? landBalance.toNumber() : "..."}</td>
-  //     </tr>
-  //     <tr>
-  //       <td>Total LAND Supply</td>
-  //       <td>{landSupply ? landSupply.toNumber() : "..."}</td>
-  //     </tr>
-  //     <tr>
-  //       <td>FRUIT</td>
-  //       <td>{fruitBalance ? utils.formatEther(fruitBalance) : "..."}</td>
-  //     </tr>
-  //     <tr>
-  //       <td>Total FRUIT Supply</td>
-  //       <td>{fruitSupply ? utils.formatEther(fruitSupply) : "..."}</td>
-  //     </tr>
-  //   </tbody>
-  // </table>
 }
